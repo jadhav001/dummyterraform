f88f9d12728ea79587e48f97ee3fc1ca6f5afd7c
jadhav001 <147920870+jadhav001@users.noreply.github.com>
2024-09-09 14:51:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,14 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "three" {
-  count         =  3
+  count         =  1
   ami           = "ami-00b8917ae86a424c9"
-  instance_type = "t2.medium"
-  key_name      = "mykeypair9am"
+  instance_type = "t2.micro"
   tags = {
     Name = "mumbai-server"
   }
 }
 
 resource "aws_s3_bucket" "abcd" {
-bucket = "devopsherahamshaik0099889977hcp"
+bucket = "devopshehcp"
 }
